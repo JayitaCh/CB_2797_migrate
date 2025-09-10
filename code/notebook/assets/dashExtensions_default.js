6242e48eb0b36d8282c58ce6da3c2147c1b81c75
Jayita Chakraborty <J.Chakraborty@leeds.ac.uk>
2025-09-10 18:54:47 +0100

    Finalised Dash layout

--- code/notebook/assets/dashExtensions_default.js
+++ code/notebook/assets/dashExtensions_default.js
@@ -1,36 +1,20 @@
 window.dashExtensions = Object.assign({}, window.dashExtensions, {
     default: {
-        function0: function(el) {
-                var slider = document.createElement('input');
-                slider.type = 'range';
-                slider.min = 0;
-                slider.max = 1;
-                slider.step = 0.01;
-                slider.value = 0.5;
-                slider.style.position = 'absolute';
-                slider.style.top = '10px';
-                slider.style.left = '50%';
-                slider.style.transform = 'translateX(-50%)';
-                slider.style.zIndex = 1000;
-                el.appendChild(slider);
-
-                var topLayer = el.querySelectorAll('path')[0];
-                slider.oninput = function() {
-                    topLayer.style.clip = 'rect(0px, ' + (slider.value * el.offsetWidth) + 'px, ' + el.offsetHeight + 'px, 0px)';
-                }
+        function0: L.control.sideBySide,
+        function1: function() {
+            L.control.sideBySide(this._layers[0], this._layers[1]).addTo(this._map);
+        },
+        function2: function(map, context) {
+                const left = context.refs.left;
+                const right = context.refs.right;
+                L.control.sideBySide(left, right).addTo(map);
             }
 
             ,
-        function1: function(feature) {
-            return feature.properties.style;
-        },
-        function2: function(feature, layer) {
-            layer.bindTooltip(feature.properties.tooltip);
-        },
-        function3: function(map, ctx) {
-            var leftLayer = ctx.refs.left;
-            var rightLayer = ctx.refs.right;
-            L.control.sideBySide(leftLayer, rightLayer).addTo(map);
+        function3: function() {
+            const leftlayer = this.refs.left;
+            const rightlayer = this.refs.right;
+            L.control.sideBySide(leftlayer, rightlayer).addTo(this.map);
         }
 
     }
